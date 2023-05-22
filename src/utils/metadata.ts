c90f67cb56a7aed72c335f438ec8f56c4fcccaa4
Sami Warraich <samiwarraich51@gmail.com>
2023-05-22 19:00:29 +0300

    change meta data

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,29 +1,29 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Color Gradient",
+  title: "Pixel Muse",
   description:
-    "Color Gradient is a suite of bots that create and share gradient color images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
-  metadataBase: new URL("https://colorgradient.vercel.app"),
+    "Pixel Muse is a suite of bots that create and share random color pixel images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
+  metadataBase: new URL("https://pixel-muse.vercel.app"),
   twitter: {
     card: "summary_large_image",
-    title: "Color Gradient",
+    title: "Pixel Muse",
     description:
-      "Color Gradient is a suite of bots that create and share gradient color images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
-    site: "@gradient_bot",
+      "Pixel Muse is a suite of bots that create and share random color pixel images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
+    site: "@PixelMuseBot",
     creator: "@samiwarraich2",
-    images: "/images/color-gradient-bot.jpg",
+    images: "/images/pixel-muse-bot.jpeg",
   },
   openGraph: {
     type: "website",
-    url: "https://colorgradient.vercel.app",
-    title: "Color Gradient",
+    url: "https://pixel-muse.vercel.app",
+    title: "Pixel Muse",
     description:
-      "Color Gradient is a suite of bots that create and share gradient color images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
-    siteName: "Color Gradient",
+      "Pixel Muse is a suite of bots that create and share random color pixel images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
+    siteName: "Pixel Muse",
     images: [
       {
-        url: "/images/color-gradient-bot.jpg",
+        url: "/images/pixel-muse-bot.jpeg",
       },
     ],
   },
@@ -31,15 +31,15 @@ export const metadata: Metadata = {
   keywords: [
     "colors",
     "colour",
-    "gradient",
-    "color code",
-    "gradient background",
-    "color gradient",
-    "gradient colors",
-    "gradients",
-    "gradient generator",
-    "help with color management in windows",
-    "hex color",
+    "pixels",
+    "pixel",
+    "pixel art",
+    "random pixel",
+    "pixel muse",
+    "pixel bot",
+    "pixel muse bot",
+    "art",
+    "random color pixel art",
     "samiwarraich",
     "Sami Warraich",
   ],
