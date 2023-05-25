4525e7fe0f9839676ff84d19b40ba574963f5fc2
Sami Warraich <samiwarraich51@gmail.com>
2023-05-25 12:23:05 +0300

    add error for cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,16 @@ import { getConfig, getPhoto } from "@/services";
 export default async function Home() {
   const res = await getConfig();
   const photo = await getPhoto();
-  if ("error" in res) {
+  if ("error" in res && "error" in photo) {
     return (
       <div className="flex justify-center items-center h-screen text-xl">
-        {res.error}
+        {res.error || photo.error}
       </div>
     );
   }
   return (
-    <div className="flex justify-around">
-      <Card data={res.bots} photo={photo} />
+    <div className="flex justify-start">
+      <Card data={res} photo={photo} />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,42 @@
 import ImageCard from "./ImageCard";
 import CardItem from "./CardItem";
-import { IBot, IError, IPhoto } from "@/types";
+import { IConfig, IError, IPhoto } from "@/types";
 
 export default function Card({
   data,
   photo,
 }: {
-  data: IBot[];
+  data: IConfig | IError;
   photo: IPhoto | IError;
 }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-4">
-      <ImageCard photo={photo} />
-      {data.map(({ title, username, url, image, alt, description }, index) => (
-        <CardItem
-          key={index}
-          title={title}
-          username={username}
-          url={url}
-          image={image}
-          alt={alt}
-          description={description}
-        />
-      ))}
+    <div className="min-h-screen w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-4">
+        <ImageCard photo={photo} />
+        {"error" in data ? (
+          <div className="max-w-sm rounded overflow-hidden shadow-lg border-1 border-line">
+            <div className="relative h-80 w-full">
+              <div className="flex items-center justify-center h-full">
+                <p>{data?.error}</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          data.bots.map(
+            ({ title, username, url, image, alt, description }, index) => (
+              <CardItem
+                key={index}
+                title={title}
+                username={username}
+                url={url}
+                image={image}
+                alt={alt}
+                description={description}
+              />
+            )
+          )
+        )}
+      </div>
     </div>
   );
 }
