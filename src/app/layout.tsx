355e3a8d0ede766b59286091f52b4153c725a039
Sami Warraich <samiwarraich51@gmail.com>
2023-06-01 19:40:16 +0300

    add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 import { Header, Footer } from "@/components";
 import { firaCode } from "@/utils";
@@ -10,6 +11,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const MS_CLARITY = process.env.NEXT_PUBLIC_MS_CLARITY;
   return (
     <html lang="en" className={`${firaCode.variable} font-fira-code`}>
       <body>
@@ -17,6 +19,13 @@ export default function RootLayout({
           <Header />
           {children}
           <Analytics />
+          <Script type="text/javascript" id="ms-clarity">
+            {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${MS_CLARITY}");`}
+          </Script>
           <Footer />
         </div>
       </body>
