464e0ade67f1b4d36407068a5059c4510c19f38a
Sami Warraich <samiwarraich51@gmail.com>
2023-06-07 21:14:53 +0300

    fixes

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -26,7 +26,7 @@ const ButtonIcon = ({
               isRotate ? "rotate-180 transition-transform duration-1000" : ""
             }`
           : "transition-transform duration-200 hover:shadow-xl hover:-translate-y-1 focus-within:shadow-xl focus-within:-translate-y-1 focus:outline-none cursor-pointer"
-      } `}
+      }`}
     >
       <Icon size={24} />
     </button>
