ab6a2aa90a994950e301e8801ed1bdb5ce6081af
Sami Warraich <samiwarraich51@gmail.com>
2023-06-12 20:42:59 +0300

    fixes.

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Bot } from "@/types";
+import { Loader } from "@/components";
 
 const CardItem = ({
   title,
@@ -16,13 +17,17 @@ const CardItem = ({
     <div className="max-w-sm rounded overflow-hidden shadow-lg border-[1px] border-dashed border-[#9ba4b5] transition-transform duration-200 hover:shadow-xl hover:-translate-y-1 focus-within:shadow-xl focus-within:-translate-y-1 cursor-pointer backdrop-brightness-125">
       <Link target="_blank" href={url}>
         <div className="relative h-52 w-full">
-          <Image
-            src={image}
-            alt={alt}
-            layout="fill"
-            objectFit="cover"
-            priority
-          />
+          {image ? (
+            <Image
+              src={image}
+              alt={alt}
+              layout="fill"
+              objectFit="cover"
+              priority
+            />
+          ) : (
+            <Loader />
+          )}
         </div>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">{title}</div>
