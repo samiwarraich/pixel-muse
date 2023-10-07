6af4b017bda9ea025e9f4e9a5643600dd783821b
Sami Warraich <samiwarraich51@gmail.com>
2023-10-07 21:11:34 +0300

    change copyright text in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 function Footer() {
   return (
     <footer className="px-4 py-8">
-      <div className="mx-auto text-center">{`© ${new Date().getFullYear()} All rights reserved.`}</div>
+      <div className="mx-auto text-center">{`© ${new Date().getFullYear()} Pixel Muse.`}</div>
     </footer>
   );
 }
