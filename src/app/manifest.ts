097735348696fa699b401658242a80826d5bf741
Sami Warraich <samiwarraich51@gmail.com>
2023-10-16 13:59:35 +0300

    add manifest file

---
+++ src/app/manifest.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Pixel Muse",
+    short_name: "Pixel Muse",
+    description:
+      "Pixel Muse is a suite of bots that create and share random color pixel images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#1C2331",
+    theme_color: "#1C2331",
+    icons: [
+      {
+        src: "./favicon.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "./favicon.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+  };
+}
