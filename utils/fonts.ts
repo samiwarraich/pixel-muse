2260013886a13e74d84b6474ace18176b74a7555
Sami Warraich <samiwarraich51@gmail.com>
2023-10-24 19:34:28 +0300

    refactor

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -3,23 +3,23 @@ import localFont from "next/font/local";
 export const firaCode = localFont({
   src: [
     {
-      path: "../../public/fonts/FiraCode-Light.ttf",
+      path: "../public/fonts/FiraCode-Light.ttf",
       weight: "300",
     },
     {
-      path: "../../public/fonts/FiraCode-Regular.ttf",
+      path: "../public/fonts/FiraCode-Regular.ttf",
       weight: "400",
     },
     {
-      path: "../../public/fonts/FiraCode-Medium.ttf",
+      path: "../public/fonts/FiraCode-Medium.ttf",
       weight: "500",
     },
     {
-      path: "../../public/fonts/FiraCode-SemiBold.ttf",
+      path: "../public/fonts/FiraCode-SemiBold.ttf",
       weight: "600",
     },
     {
-      path: "../../public/fonts/FiraCode-Bold.ttf",
+      path: "../public/fonts/FiraCode-Bold.ttf",
       weight: "700",
     },
   ],
