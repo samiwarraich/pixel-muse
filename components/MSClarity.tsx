3f78f759792eedd1c5ffff55908ece135269562d
Sami Warraich <samiwarraich51@gmail.com>
2023-10-25 12:22:16 +0300

    lazy load the clarity

--- components/MSClarity.tsx
+++ components/MSClarity.tsx
@@ -5,11 +5,7 @@ function MSClarity() {
   if (!MS_CLARITY) return null;
   return (
     <>
-      <Script
-        type="text/javascript"
-        id="ms-clarity"
-        strategy="afterInteractive"
-      >
+      <Script type="text/javascript" id="ms-clarity" strategy="lazyOnload">
         {`(function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
