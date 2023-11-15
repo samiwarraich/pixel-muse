711276c85234752e1421dd3dd86a86d8d3c14f82
Sami Warraich <samiwarraich51@gmail.com>
2023-11-15 14:53:21 +0300

    refactor metadata verification

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -45,11 +45,13 @@ export const metadata: Metadata = {
     "samiwarraich",
     "Sami Warraich",
   ],
-  other: {
-    "google-site-verification": "hztt4vcG42duVjOSpid_W9ugB5xznC01VRcutNPlBpI",
-    "msvalidate.01": "DD992E9CC85A0E458EA3599D0526C78A",
-    "p:domain_verify": "5d40bb6cf120718969b9be1519543e7e",
-    "yandex-verification": "69c311bc88f7a5d1",
+  verification: {
+    google: "hztt4vcG42duVjOSpid_W9ugB5xznC01VRcutNPlBpI",
+    yandex: "69c311bc88f7a5d1",
+    other: {
+      "msvalidate.01": "DD992E9CC85A0E458EA3599D0526C78A",
+      "p:domain_verify": "5d40bb6cf120718969b9be1519543e7e",
+    },
   },
   icons: [
     {
