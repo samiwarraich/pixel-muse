e77594f0abf4ef2ed83c0752bedd1512e4eaf3a2
Sami Warraich <samiwarraich51@gmail.com>
2023-12-17 12:23:02 +0300

    fix same color issue

--- components/ImageActionPanel.tsx
+++ components/ImageActionPanel.tsx
@@ -37,7 +37,7 @@ const ImageActionPanel = ({
       <div className="flex justify-center items-center">
         {colors.map((color, index) => (
           <ColorPickerIcon
-            key={index}
+            key={`${index}-${color.hex}`}
             color={color.hex}
             pickerIndex={index}
             toggle={toggleColorPicker}
