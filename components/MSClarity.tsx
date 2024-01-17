c9071afef8234575c13c8b003f1389e4f7329170
Sami Warraich <samiwarraich51@gmail.com>
2024-01-17 13:36:24 +0300

    fixes

--- components/MSClarity.tsx
+++ components/MSClarity.tsx
@@ -5,15 +5,13 @@ const MSClarity = () => {
   const { MS_CLARITY } = config;
   if (!MS_CLARITY) return null;
   return (
-    <>
-      <Script type="text/javascript" id="ms-clarity" strategy="lazyOnload">
-        {`(function(c,l,a,r,i,t,y){
+    <Script type="text/javascript" id="ms-clarity" strategy="lazyOnload">
+      {`(function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "${MS_CLARITY}");`}
-      </Script>
-    </>
+    </Script>
   );
 };
 
