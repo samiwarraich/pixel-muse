5e79456fc0dab4039a1a47cdf16ffc0a28d7a8be
Sami Warraich <samiwarraich51@gmail.com>
2024-09-17 20:41:49 +0300

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -8,8 +8,8 @@ export default function manifest(): MetadataRoute.Manifest {
       "Pixel Muse is a suite of bots that create and share random color pixel images on Twitter and Telegram platforms. You can enjoy these beautiful images every day by following the bots on social media or requesting them via messages or mentions.",
     start_url: "/",
     display: "standalone",
-    background_color: "transparent",
-    theme_color: "red",
+    background_color: "#ffffff",
+    theme_color: "transparent",
     icons: [
       {
         src: "/images/favicon.png",
